5194f2d6731a17432853d8230f9e0096cf78d477
Damodharan12344 <144426351+Damodharan12344@users.noreply.github.com>
2023-09-20 15:41:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Please enter a positive value.")
-
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        elif amount <= 0:
-            print("Invalid withdrawal amount. Please enter a positive value.")
-        else:
-            print("Insufficient funds for withdrawal.")
-
-    def display_balance(self):
-        print(f"Account Holder: {self.__account_holder_name}")
-        print(f"Account Number: {self.__account_number}")
-        print(f"Account Balance: ${self.__account_balance}")
-
-
-# Testing the BankAccount class
-if __name__ == "__main__":
-    account = BankAccount("123456789", "John Doe", 1000)
-
-    account.display_balance()
-    account.deposit(500)
-    account.withdraw(200)
-    account.withdraw(1500)
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+number=int(input("enter the value:"))
+res=fact_rec(number)
+print("the factorial of {} is {}".format(number,res))
